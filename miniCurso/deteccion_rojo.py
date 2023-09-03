45c55105c3eeb907b79d31e58ace637ca40fc777
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-09-03 18:25:53 -0300

    no anda

--- miniCurso/deteccion_rojo.py
+++ miniCurso/deteccion_rojo.py
@@ -6,26 +6,28 @@ cap = cv.VideoCapture(0) # Abrir una cámara web
 # Configuro calidad de video
 nuevo_ancho = 720
 nuevo_alto = 480
-video = cv.resize(cap, (nuevo_ancho, nuevo_alto))
 
 # Cambia el tamaño de la ventana de visualización
 cv.namedWindow("Ventana de Visualizacion", cv.WINDOW_NORMAL)  # Establece la ventana en modo redimensionable
-
 # Rangos de Colores
 #Rojo
-rojo_bajo1 = np.array([], np.uint8)
-rojo_alto1 = np.array([], np.uint8)
+rojo_bajo1 = np.array([0,100,255], np.uint8)
+rojo_alto1 = np.array([7,100,255], np.uint8)
 
-rojo_bajo2 = np.array([], np.uint8)
-rojo_alto2 = np.array([], np.uint8)
+rojo_bajo2 = np.array([175,100,255], np.uint8)
+rojo_alto2 = np.array([179,100,255], np.uint8)
 
 while True:
     hayvideo, video = cap.read() # Obtengo video en vivo si hayvideo es true => hay video
+    video_redimensionado = cv.resize(video, (nuevo_ancho, nuevo_alto))
 
     if hayvideo == True:
-        videoHSV = cv.cvtColor(video, cv.COLOR_BGR2HSV)
-        mascara_rojo1 = 
-        mascara_rojo2 = 
+        videoHSV = cv.cvtColor(video_redimensionado, cv.COLOR_BGR2HSV)
+        mascara_rojo1 = cv.inRange( videoHSV, rojo_bajo1, rojo_alto1)
+        mascara_rojo2 = cv.inRange( videoHSV, rojo_bajo2, rojo_alto2)
+        mascara_rojo = cv.add(mascara_rojo1, mascara_rojo2)
+
+        cv.imshow('Ventana para ver resultado', mascara_rojo)
         cv.imshow("Ventana de Visualizacion", video)
         if cv.waitKey(1) & 0xFF == ord('s'): # Si es presionada durante 1ms la tecla 'S' ord() devuelve el valor que es comparado con waitKey(1) y el programa sale del while
             break
