e31e15ac69ffadd1b0443614c6926c6e0172b826
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-09-03 17:50:30 -0300

    falta establecer rangos colores

--- miniCurso/deteccion_rojo.py
+++ miniCurso/deteccion_rojo.py
@@ -3,12 +3,30 @@ import numpy as np
 
 cap = cv.VideoCapture(0) # Abrir una cámara web
 
+# Configuro calidad de video
+nuevo_ancho = 720
+nuevo_alto = 480
+video = cv.resize(cap, (nuevo_ancho, nuevo_alto))
+
+# Cambia el tamaño de la ventana de visualización
+cv.namedWindow("Ventana de Visualizacion", cv.WINDOW_NORMAL)  # Establece la ventana en modo redimensionable
+
+# Rangos de Colores
+#Rojo
+rojo_bajo1 = np.array([], np.uint8)
+rojo_alto1 = np.array([], np.uint8)
+
+rojo_bajo2 = np.array([], np.uint8)
+rojo_alto2 = np.array([], np.uint8)
 
 while True:
-    hayvideo, video = cap.read()
+    hayvideo, video = cap.read() # Obtengo video en vivo si hayvideo es true => hay video
 
     if hayvideo == True:
-        cv.imshow(video, "display")
+        videoHSV = cv.cvtColor(video, cv.COLOR_BGR2HSV)
+        mascara_rojo1 = 
+        mascara_rojo2 = 
+        cv.imshow("Ventana de Visualizacion", video)
         if cv.waitKey(1) & 0xFF == ord('s'): # Si es presionada durante 1ms la tecla 'S' ord() devuelve el valor que es comparado con waitKey(1) y el programa sale del while
             break
 
