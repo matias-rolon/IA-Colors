15d1d97f9d3e7ba7db24a0958d8330dec5d1285c
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-09-07 23:44:59 -0300

    me voy a dormir ya no entiendo

--- deteccion de objeto/main.py
+++ deteccion de objeto/main.py
@@ -5,21 +5,22 @@ DEBUG_HSV = 0
 cap = cv.VideoCapture(0)
 
 # Cambia el tamaño de la ventana de visualización
-cv.namedWindow("Ventana de Visualizacion", cv.WINDOW_NORMAL)  # Establece la ventana en modo redimensionable
+cv.namedWindow("Ventana de Capturacion de objeto", cv.WINDOW_NORMAL)  # Establece la ventana en modo redimensionable
 
 
 while True:
-    hayvideo, video = cap.read() # Obtengo video en vivo si hayvideo es true => hay video
-    if hayvideo:
-        hsv_video = cv.cvtColor(video ,cv.COLOR_BGR2HSV) # paso el video a HSV(HUE SATURATION VALUE)
-
-        #MUESTRO POR VENTANA
-        cv.imshow("Ventana de Visualizacion", video)
-        if DEBUG_HSV == True :
-            cv.imshow('Ventana video ', hsv_video)
-
-        if cv.waitKey(1) == ord('esc'): # Si es presionada durante 1ms la tecla 'S' ord() devuelve el valor que es comparado con waitKey(1) y el programa sale del while
+    hayvideo, video1 = cap.read() # Obtengo video en vivo si hayvideo es true => hay video
+    cv.imshow("Ventana de Capturacion de objeto", video1)  #MUESTRO POR VENTANA
+    if cv.waitKey(1) == ord('c'): # Si es presionada durante 1ms la tecla 'S' ord() devuelve el valor que es comparado con waitKey(1) y el programa sale del while
             break
+    
+# Leemos ultima img y la guardamos
+objeto = cv.imread('objeto1.jpg', 0)
+recorte= objeto[160:300, 230:380]
+cv.imshow(" recorte de objeto",recorte)
 
+while True:
+    hayvideo, video2 = cap.read() # Obtengo video en vivo si hayvideo es true => hay video
+    cv.imshow("Deteccion", video2)  #MUESTRO POR VENTANA
 cap.release()# Liberar la captura de video y cerrar la cámara
 cv.destroyAllWindows() # Cerrar cualquier ventana abierta
